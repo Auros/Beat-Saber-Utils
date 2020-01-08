9a1b8322d94ba51310fa417084f561743d2e3fbe
Auros Nexus <aurosnex@gmail.com>
2020-01-07 23:58:31 -0500

    Cleaned up the logger
    nice and bubbly

--- Beat Saber Utils/Utilities/Logger.cs
+++ Beat Saber Utils/Utilities/Logger.cs
@@ -1,28 +1,16 @@
 using System;
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using IPALogger = IPA.Logging.Logger;
+
 namespace BS_Utils.Utilities
 {
     public class Logger
     {
-
         internal static IPALogger log { get; set; }
-        public static void Log(string modName, string message)
-        {
-            log.Info($"[{modName}]  {message}");
-        }
 
-        internal static void Log(string message)
-        {
-            log.Info(message);
-        }
-        internal static void Log(string message, IPALogger.Level level)
-        {
-            log.Log(level, message);
-        }
+        internal static void Log(string message) => log.Info(message);
+        internal static void Log(string message, IPALogger.Level level) => log.Log(level, message);
+
+        [Obsolete("This is legacy code from the old IPA days, please use your own logger")]
+        public static void Log(string modName, string message) => log.Info($"[{modName}]  {message}");
     }
 }
